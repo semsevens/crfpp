4f3759453d8d95d91fcf68d9c0a554179f5b2479
yangxiaohui04 <yangxiaohui04@meituan.com>
2018-12-05 13:31:06 +0800

    add mac filter

--- python/setup.py
+++ python/setup.py
@@ -3,17 +3,27 @@
 from distutils.core import setup,Extension,os
 from os.path import expanduser
 import string
+import platform
+
+IS_MAC = 'Darwin' in platform.platform()
 HOME = expanduser("~")
 CRF_PATH = HOME + '/local/CRF++'
 INCLUDE_PATH = CRF_PATH + '/include'
 LIB_PATH = CRF_PATH + '/lib'
+
+extra_compile_args = []
+extra_link_args=[]
+if IS_MAC:
+    extra_compile_args.append('-mmacosx-version-min=10.9')
+    extra_link_args.append('-stdlib=libc++')
+
 setup(name = "mecab-python",
       py_modules=["CRFPP"],
       ext_modules = [Extension("_CRFPP",
                                ["CRFPP_wrap.cxx",],
                                include_dirs = [INCLUDE_PATH],
                                library_dirs = [LIB_PATH],
-                               extra_compile_args=['-mmacosx-version-min=10.9'],
-                               extra_link_args=['-stdlib=libc++'],
+                               extra_compile_args = extra_compile_args,
+                               extra_link_args = extra_link_args,
                                libraries=["crfpp", "pthread"])
                      ])
